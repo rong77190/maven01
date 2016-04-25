541bc113c00266d0e1e5c41a7fab270916d72b96
xiechur <18826077190@163.com>
2016-04-25 23:46:02 +0800

    from bash

--- src/main/java/com/xiechur/service/impl/StudentServiceImpl.java
+++ src/main/java/com/xiechur/service/impl/StudentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.xiechur.service.impl;
 
+import com.xiechur.dao.StudentDaoI;
 import com.xiechur.pageModel.Student;
 import com.xiechur.service.StudentServiceI;
 import org.apache.log4j.Logger;
@@ -11,10 +12,18 @@ import java.util.List;
  */
 public class StudentServiceImpl implements StudentServiceI {
     private static final Logger logger = Logger.getLogger(StudentServiceImpl.class);
+    private StudentDaoI studentDaoI;
+
+    public StudentDaoI getStudentDaoI() {
+        return studentDaoI;
+    }
+    public void setStudentDaoI(StudentDaoI studentDaoI) {
+        this.studentDaoI = studentDaoI;
+    }
 
     @Override
     public List<Student> getAllStudent() {
-        //return studentDaoI.getAllStudent();
+//        return studentDaoI.getAllStudent();
         return null;
     }
 }
---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dell on 2016/4/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
